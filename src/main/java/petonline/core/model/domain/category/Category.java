edac84e2174d994dd56afc79050c05527a3137a5
pedro <pedro.ciria@totvspartners.com.br>
2020-07-10 00:07:59 -0300

    Ajusting Enum on Rest

--- src/main/java/petonline/core/model/domain/category/Category.java
+++ src/main/java/petonline/core/model/domain/category/Category.java
@@ -1,6 +1,8 @@
 package petonline.core.model.domain.category;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +13,24 @@ import lombok.NoArgsConstructor;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum Category {
 
-    TOYS(1,"Toys"),
-    FOOD(2,"Food"),
-    BATH(3,"Bath"),
-    ACCESSORIES(4,"Accessories"),
-    MISC(5,"Miscellaneous");
+    TOYS(1, "Toys"),
+    FOOD(2, "Food"),
+    BATH(3, "Bath"),
+    ACCESSORIES(4, "Accessories"),
+    MISC(5, "Miscellaneous");
 
 
     private Integer type;
     private String name;
+
+    @JsonCreator
+    public static Category forValues(@JsonProperty("type") Integer type,
+                                     @JsonProperty("name") String name) {
+        for (Category distance : Category.values()) {
+            if (distance.type.equals(type) && distance.name.equalsIgnoreCase(name)) {
+                return distance;
+            }
+        }
+        return null;
+    }
 }
